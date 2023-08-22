2723c5daae11cfe993089b064fb8326a6da9b07f
seheerst <seherst9@gmail.com>
2023-08-22 22:27:09 +0300

    feat: created connection string

--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -1,5 +1,23 @@
+using BlogApp.Data.Concrete.EfCore;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<BlogContext>(options =>
+{
+    var config = builder.Configuration;
+    var connectionString = config.GetConnectionString("mysql_connection");
+    //options.UseSqlite(connectionString);
+
+    var version = new MySqlServerVersion(new Version(8, 0, 34));
+
+    options.UseMySql(connectionString, version);
+});
+
 var app = builder.Build();
+
+SeedData.InitialData(app);
 
 app.MapGet("/", () => "Hello World!");
 
