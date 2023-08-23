0396585a2861e2a6e4346a083c0631871fc69b99
seheerst <seherst9@gmail.com>
2023-08-23 23:28:41 +0300

    feat: added user property

--- BlogApp/Entity/Comment.cs
+++ BlogApp/Entity/Comment.cs
@@ -11,4 +11,8 @@ public class Comment
     public int PostId { get; set; }
 
     public Post Post { get; set; } = null!;
+
+    public int UserId { get; set; }
+
+    public User User { get; set; } = null!;
 }
