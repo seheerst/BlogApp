5f6213edf977dfb5f72cb19f986e08218f898e23
seheerst <seherst9@gmail.com>
2023-08-25 21:00:40 +0300

    feat: created model

---
+++ BlogApp/Models/PostViewModel.cs
@@ -0,0 +1,11 @@
+using BlogApp.Entity;
+
+namespace BlogApp.Models
+{
+    public class PostViewModel
+    {
+        public List<Post> Posts { get; set; } = new();
+        public List<Tag> Tags { get; set; } = new();
+    }
+}
+
