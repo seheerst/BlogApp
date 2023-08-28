6ccb899ecf35c4b81b6b9dc4b378051558fe8ba9
seheerst <seherst9@gmail.com>
2023-08-28 16:15:19 +0300

    feat: added url property

--- BlogApp/Entity/Post.cs
+++ BlogApp/Entity/Post.cs
@@ -8,6 +8,9 @@ public class Post
 
     public string? Content { get; set; }
 
+    
+    public string? Url { get; set; }
+
     public string? Image { get; set; }
 
     public DateTime PublishedOn { get; set; }
