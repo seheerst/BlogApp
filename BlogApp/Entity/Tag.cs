e79c24095ab601d24f44b39465e9521de1d3fc54
seheerst <seherst9@gmail.com>
2023-09-10 22:16:36 +0300

    feat: added new column

--- BlogApp/Entity/Tag.cs
+++ BlogApp/Entity/Tag.cs
@@ -1,11 +1,17 @@
 ﻿namespace BlogApp.Entity;
 
+public enum TagColors
+{
+    primary, danger, warning,success,secondary
+}
+
 public class Tag
 {
     public int TagId { get; set; }
 
     public string? Text { get; set; }
     
+    public string? Color { get; set; }
     public string? Url { get; set; }
 
     public List<Post> Posts { get; set; } = new List<Post>();
