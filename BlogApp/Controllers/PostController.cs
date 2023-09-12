7af19e02a5f9d6d2160d024d7fdb7a9d660e4852
seheerst <seherst9@gmail.com>
2023-09-12 18:58:29 +0300

    feat: added comments to controller

--- BlogApp/Controllers/PostController.cs
+++ BlogApp/Controllers/PostController.cs
@@ -1,4 +1,5 @@
 using BlogApp.Data.Abstract;
+using BlogApp.Entity;
 using BlogApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,26 +10,59 @@ namespace BlogApp.Controllers
     public class PostController : Controller
     {
         private IPostRepository _postRepository;
-     
+        private ICommentRepository _commentRepository;
 
-        public PostController(IPostRepository postRepository)
+        public PostController(IPostRepository postRepository, ICommentRepository commentRepository)
         {
             _postRepository = postRepository;
+            _commentRepository = commentRepository;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(string? tag)
         {
+          
+            var posts = _postRepository.Posts;
+            
+            if (!string.IsNullOrEmpty(tag))
+            {
+                posts = posts.Where(x => x.Tags.Any(t => t.Url == tag));
+            }
             return View(
-                new PostViewModel
-                {
-                    Posts = _postRepository.Posts.ToList()
-                }
+                new PostViewModel{Posts = await posts.ToListAsync()}
             );
         }
 
         public async Task<IActionResult> Details(string? url)
         {
-            return View(await _postRepository.Posts.FirstOrDefaultAsync(p=> p.Url== url));
+            return View(await _postRepository.Posts
+                .Include(x=> x.Tags)
+                .Include(x=> x.Comments).ThenInclude(
+                    x=>x.User)
+                .FirstOrDefaultAsync(p=> p.Url== url));
+        }
+
+        public JsonResult AddComment(int PostId, string UserName, string Text)
+        {
+            var entity = new Comment
+            {
+                Text = Text,
+                PublishedOn = DateTime.Now,
+                PostId = PostId,
+                User = new User
+                {
+                    Name = UserName,
+                    Image = "avatar.jpg"
+                }
+            };
+            _commentRepository.CreateComment(entity);
+
+            return Json(new
+            {
+                UserName,
+                Text,
+                entity.PublishedOn,
+                entity.User.Image
+            });
         }
 
 
