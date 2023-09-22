dcf12557637e2255ac863544f87e104ac750f165
seheerst <seherst9@gmail.com>
2023-09-22 15:46:17 +0300

    feat: initialize repositories

--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -1,6 +1,7 @@
 using BlogApp.Data.Abstract;
 using BlogApp.Data.Concrete;
 using BlogApp.Data.Concrete.EfCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,6 +17,10 @@ builder.Services.AddDbContext<BlogContext>(options =>
 
 builder.Services.AddScoped<IPostRepository, EfPostRepository>();
 builder.Services.AddScoped<ITagRepository, EfTagRepository>();
+builder.Services.AddScoped<ICommentRepository, EfCommentRepository>();
+builder.Services.AddScoped<IUserRepository, EfUserRepository>();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
 
 var app = builder.Build();
 app.UseStaticFiles();
@@ -24,13 +29,23 @@ SeedData.InitialData(app);
 
 app.MapControllerRoute(
     name: "default",
-    pattern:"{controller=home}/{action=Index}/{id?}"
+    pattern:"{controller=Post}/{action=Index}/{id?}"
     );
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseRouting();
 
 app.MapControllerRoute(
     name: "post_detail",
-    pattern:"posts/{url}",
+    pattern:"posts/detail/{url}",
     defaults: new {controller="Post", action = "Details",}
+);
+
+app.MapControllerRoute(
+    name: "posts_by_tag",
+    pattern:"posts/tag/{tag}",
+    defaults: new {controller="Post", action = "Index",}
 );
 
 app.Run();
